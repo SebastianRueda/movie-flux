2d8ed4c380ea9f9caeb2b5260f51723d1940c715
Sebastian <sebastianruedaolarte@gmail.com>
2021-03-13 13:09:10 -0300

    base, homefragment and detailfragment

--- app/src/main/java/com/movieflux/iu/MainActivity.kt
+++ app/src/main/java/com/movieflux/iu/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.movieflux
+package com.movieflux.iu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.movieflux.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
